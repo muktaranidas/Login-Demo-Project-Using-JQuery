2892ce975c390a1e457ca81a136f421201879463
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 23:12:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$("#loginButton").click(function () {
+  var password1 = $("#pass1").val();
+  var password2 = $("#pass2").val();
+
+  if (password1 != "" && password2 != "") {
+    if (password1 === password2) {
+      alert("Successfully Login");
+    } else {
+      alert("Password Mismatch");
+    }
+  } else {
+    alert("Please Enter Password");
+  }
+});
